823a6907bc717162a7752df88d1b569cafcbd8b5
Chirag Poojary <143872840+chiragits@users.noreply.github.com>
2025-02-27 04:12:56 +0000

    gfghft

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <input type="text" placeholder="Type something"
+      value={text}
+      onChange={(e)=>setText(e.target.value)}
+      />
+      <h1><strong>You typed :{text}</strong></h1>
+      <h2>Text length:{text.length}</h2>
+      </>
+  );
 }
-
 export default App
